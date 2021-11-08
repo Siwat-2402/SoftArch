7b541503d129a71ba469c6265e27f52b359f7d1e
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2021-11-08 19:22:51 +0700

    Add files via upload

---
+++ SR/Behavior_2/src/WeightStrategy.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class WeightStrategy implements SortStrategy {
+    @Override
+    public void customSort(List<Person> people) {
+        people.sort(Comparator.comparingInt(Person::getWeight));
+    }
+}
